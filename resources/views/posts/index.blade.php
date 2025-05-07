627d8b8e46b52852c8f3557cf7b58dc3267a2842
hamachiii <hamasachi1996@gmail.com>
2025-05-07 15:15:54 +0900

    修正３

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,13 +29,20 @@
         <p>{{session('flash_message')}}</p>
        @endif
 
+       @if (session('error_message'))
+         <p>{{session('error_message')}}</p>
+       @endif
+
+       <a href="{{ route('posts.create') }}">新規投稿</a>
+
        @if($posts->isNotEmpty())
            @foreach($posts as $post)
                <article>
                    <h2>{{ $post->title }}</h2>
                    <p>{{ $post->content }}</p>
                    <a href="{{ route('posts.show', $post) }}">詳細</a>
-               </article>
+                   <a href="{{ route('posts.edit', $post) }}">編集</a>
+                  </article>
            @endforeach
        @else
            <p>投稿はありません。</p>
